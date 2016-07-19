abe44452e3b89d3ae3f8fc0d763fe4da70c44710
Jorge Juarez <contacto@jorgejuarez.net>
2016-07-19 14:37:55 -0500

    Form

--- contact.php
+++ contact.php
@@ -1,73 +1,69 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Settings 
-|--------------------------------------------------------------------------
-*/
 
-define("EMAIL" , "contacto@thinkersinmotion.mx");
-define("SUBJECT" , "contacto de la web");
+if($_POST)
+{
+//	$to_Email   	= "ideaspartymx@gmail.com"; //Replace with recipient email address
+	$to_Email   	= "jjuarez007@gmail.com"; //Replace with recipient email address
+	$subject        = 'Registro ideaspartymx'; //Subject line for emails
 
-define("NAME_MSG" , "Please insert your name");
-define("EMAIL_MSG" , "Please insert an email");
-define("EMAIL_WRONG" , "Please insert a valid email");
-define("PHONE_MSG" , "Please insert a phonenumber");
-define("MESSAGE_MSG" , "Please insert a message");
+	//check if its an ajax request, exit if not
+    if(!isset($_SERVER['HTTP_X_REQUESTED_WITH']) AND strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest') {
 
-/*
-|--------------------------------------------------------------------------
-| Simple Sender Script
-|--------------------------------------------------------------------------
-*/
+		//exit script outputting json data
+		$output = json_encode(
+		array(
+			'type'=>'error',
+			'text' => 'Request must come from Ajax'
+		));
 
-if( $_POST ) {
-    
-	/* check mandatory fields */
-	if( empty( $_POST['name'] ) ) {
-		exit( NAME_MSG );
-	}
-	
-	if( empty( $_POST['email'] ) ) {
-		exit( EMAIL_MSG );
-	}
-	
-	if( empty( $_POST['phone'] ) ) {
-		exit( PHONE_MSG );
-	}
-	
-	if( empty( $_POST['message'] ) ) {
-		exit( MESSAGE_MSG );
-	}
-	
-	/* validate email */
-	if ( !empty( $_POST['email']) && !preg_match('/^[^0-9][a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[@][a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[.][a-zA-Z]{2,4}$/', $_POST['email'])) {
-		
-		exit( EMAIL_WRONG );
-		
-	}
-	
-	/* create body message */
-    $message = '';
-    
-	// Name 
-	$message.= '<p> Name : ' . $_POST['name'] . '</p>';
-	
-    // Phone
-    $message.= '<p> Phone : ' . $_POST['phone'] . '</p>';
-	    
-    // Message
-    $message.= '<p> Message : ' . $_POST['message'] . '</p>';
-	
-     
-/* send email */
-$email = mail( EMAIL , SUBJECT , $message , "From: ".$_POST['name']." <".$_POST['email'].">\r\n" ."Reply-To: ".$_POST['email']."\r\n" );
+		die($output);
+    }
 
-/* callback for ajax */
-if( $email ) {
-    echo 'OK';
-} else { 
-    echo 'ERROR';	}
+	//check $_POST vars are set, exit if any missing
+	if(!isset($_POST["nombre"])
+		|| !isset($_POST["edad"])
+		|| !isset($_POST["ciudad"])
+		|| !isset($_POST["email"])
+		|| !isset($_POST["codigo"]))
+	{
+		$output = json_encode(array('type'=>'error', 'text' => 'Por favor rellena todos los campos!'));
+		die($output);
+	}
+
+	//Sanitize input data using PHP filter_var().
+	$user_Name        = filter_var($_POST["nombre"], FILTER_SANITIZE_STRING);
+	$user_Email       = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
+	$edad        = filter_var($_POST["edad"], FILTER_SANITIZE_STRING);
+	$ciudad        = filter_var($_POST["ciudad"], FILTER_SANITIZE_STRING);
+	$codigo        = filter_var($_POST["codigo"], FILTER_SANITIZE_STRING);
+	// $user_Type        = filter_var($_POST["userType"], FILTER_SANITIZE_STRING);
+
+
+	$message ="Hola mi nombre es ".$user_Name.". Tengo ".$edad.". Soy de: ".$ciudad.". ";
+	$message .= "Ya he adquirido mi boleto en algun punto de venta y voy a terminar mi registo. ";
+	$message .= "Mi correo electronico es: ".$user_Email.". ";
+	$message .= "El codigo de mi boleto es: ".$codigo.". Nos vemos este 20 de Octubre. Hasta Pronto!";
+
+	// $message .="User Type: $user_Type\n";
+
+
+
+	//proceed with PHP email.
+	$headers = 'From: '.$user_Email.'' . "\r\n" .
+	'Reply-To: '.$user_Email.'' . "\r\n" .
+	'X-Mailer: PHP/' . phpversion();
+
+	$sentMail = @mail($to_Email, $subject, $message, $headers);
+	$sentMail = @mail("ideaspartymx@gmail.com", $subject, $message, $headers);
+
+	if(!$sentMail)
+	{
+		$output = json_encode(array('type'=>'error', 'text' => 'Could not send mail! Please check your PHP mail configuration.'));
+		die($output);
+	}else{
+		$output = json_encode(array('type'=>'message', 'text' => 'Te has registrado exitosamente'));
+		die($output);
+	}
 }
-	
 ?>
---
+++ js/_custom.js
@@ -0,0 +1,52 @@
+(function ($) {
+    //jorge Juarez
+
+    $("#submit_btn_2").click(function (e) {
+        e.preventDefault();
+
+        var _form    = $("#myForm").serializeArray();
+        var formData = {};
+        _form.forEach(function (item) {
+            formData[item.name] = item.value.toString();
+        });
+
+        // var error1 = '<div class="enter-name col-lg-3 align-center"> Enter the name </div>';
+        // jQuery("#result").hide().html(error1).fadeIn(500);
+
+
+        try {
+
+            var filter = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+            
+
+            if (formData.nombre == '') throw new Error("Ingresa tu Nombre");
+            if (formData.edad == '') throw new Error("Ingresa tu Edad");
+            if (formData.ciudad == '') throw new Error("Ingresa tu Ciudad");
+            if (formData.correo == '') throw new Error("Ingresa tu Correo");
+            if (!filter.test(formData.correo)) throw new Error("Correo Invalido");
+            if (formData.codigo == '') throw new Error("Ingresa tu Codigo");
+
+
+            $.post("/contact.php", formData, function (response) {
+                var output;
+                if (response.type == 'error') {
+                    output = '<div class="error col-lg-3 align-center">' + response.text + '</div>';
+                } else {
+                    output = '<div class="success col-lg-3 align-center">' + response.text + '</div>';
+                }
+                $("#result").hide().html(output).fadeIn(500);
+            }, 'json');
+
+
+        } catch (ex) {
+            var error1 = '<div class="enter-name col-lg-3 align-center"> ' + ex.message + ' </div>';
+            $("#result").hide().html(error1).fadeIn(500);
+        }
+
+
+        console.log(data);
+        alert(2);
+
+    });
+
+})(jQuery);
